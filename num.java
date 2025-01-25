7c730316aab0d064701eee149ee582be8d2c8d5e
Akshitha-527 <nampallyakshitha5@gmail.com>
2025-01-25 16:06:43 +0530

    Update EvenOdd.java

--- num.java
+++ num.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class num {
+public class EvenOdd {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        System.out.println(a%2==0?"Even":"Odd");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a = sc.nextInt();
+        System.out.println(a%2 == 0 ? "Even":"Odd" );
        
     }
 }
